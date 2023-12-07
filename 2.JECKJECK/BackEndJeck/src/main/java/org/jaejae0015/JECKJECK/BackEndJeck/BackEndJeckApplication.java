63ccca7e41c93b7404c291f64b53ce0c8103581e
jaejae0015 <salangae00@naver.com>
2023-12-07 11:40:54 +0900

    SpringBoot 최초 생성 및 커밋

---
+++ 2.JECKJECK/BackEndJeck/src/main/java/org/jaejae0015/JECKJECK/BackEndJeck/BackEndJeckApplication.java
@@ -0,0 +1,13 @@
+package org.jaejae0015.JECKJECK.BackEndJeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndJeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndJeckApplication.class, args);
+	}
+
+}
---
+++ 2.JECKJECK/BackEndJeck/src/test/java/org/jaejae0015/JECKJECK/BackEndJeck/BackEndJeckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jaejae0015.JECKJECK.BackEndJeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndJeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
